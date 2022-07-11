fe96b76b78d99f029510d63905cec191ba8a98a0
Lucas Rinaldi <lucasreinaldi@gmail.com>
2022-07-11 18:45:15 -0300

    dev

--- app/pages/_components/main-nav/main-nav.component.ts
+++ app/pages/_components/main-nav/main-nav.component.ts
@@ -1,10 +1,12 @@
-import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, HostBinding } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 import { AuthService } from '@app/scripts/services/auth.service';
 import { TaskService } from '@app/scripts/services/task.service';
@@ -16,9 +18,11 @@ import { UserService } from '@app/scripts/services/user.service';
     styleUrls: ['./main-nav.component.scss'],
 })
 export class MainNavComponent implements OnInit {
+    @HostBinding('class') className = '';
     time = new Date();
     opened: boolean;
     isLogged = false;
+    toggleTheme = new FormControl(false);
 
     isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
         map((result) => result.matches),
@@ -34,12 +38,23 @@ export class MainNavComponent implements OnInit {
         public userService: UserService,
         public changeDetector: ChangeDetectorRef,
         public router: Router,
+        private overlay: OverlayContainer,
     ) {}
 
     ngOnInit(): void {
         setInterval(() => (this.time = new Date()), 1000);
         this.isLogged = this.authService.getIsAuth();
         this.authService.emitMenu.subscribe((result: boolean) => (this.isLogged = result));
+
+        this.toggleTheme.valueChanges.subscribe((darkMode) => {
+            const darkClassName = 'darkMode';
+            this.className = darkMode ? darkClassName : '';
+            if (darkMode) {
+                this.overlay.getContainerElement().classList.add(darkClassName);
+            } else {
+                this.overlay.getContainerElement().classList.remove(darkClassName);
+            }
+        });
     }
 
     logout(): void {
@@ -60,12 +75,5 @@ export class MainNavComponent implements OnInit {
         });
     }
 
-    changeTheme(language: string): void {
-        this.translateService.use(language);
-
-        this.translateService.get('messages.language-changed').subscribe((text: string) => {
-            let message = text;
-            this.snackBar.open(message, undefined, { duration: 5000 });
-        });
-    }
+    changeTheme(): void {}
 }
