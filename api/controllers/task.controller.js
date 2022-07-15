772012b80b93ee95361c88b0c7362d6e28428286
Lucas Rinaldi <lucasreinaldi@gmail.com>
2022-07-14 22:50:06 -0300

    ajusta task controller

--- api/controllers/task.controller.js
+++ api/controllers/task.controller.js
@@ -55,74 +55,59 @@ exports.findAllByUser = async (request, response) => {
 exports.findOne = async (request, response) => {
     const language = request.headers.language;
 
-    try {
-        const document = await Model.findOne({ _id: request.params._id });
-        if (!document)
-            if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.params._id}. Document name: {${Model.modelName}}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.params._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
+    const [data, error] = await httpHandler.parser(request, response, Model.findOne({ _id: request.params._id }));
+    if (!!error) return;
+    if (!data)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.params._id}. Document name: {${Model.modelName}}.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.params._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
 
-        httpHandler.success(response, document, StatusCode.SuccessOK);
-    } catch (error) {
-        if (language == 'en-US') httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-        else httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-    }
+    httpHandler.success(response, data, StatusCode.SuccessOK);
 };
 
 exports.create = async (request, response) => {
     const language = request.headers.language;
 
-    try {
-        const document = await new Model(request.body).save();
-        if (!document || document.n === 0)
-            if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error creating document. Document name: {${Model.modelName}}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao criar documento. Nome do documento: {${Model.modelName}}.`);
+    const [data, error] = await httpHandler.parser(request, response, new Model(request.body).save());
+    if (!!error) return;
+    if (!data || data.n === 0)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error creating category.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao criar categoria.`);
 
-        httpHandler.success(response, document, StatusCode.SuccessCreated);
-    } catch (error) {
-        if (language == 'en-US') httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-        else httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-    }
+    httpHandler.success(response, data, StatusCode.SuccessCreated);
 };
 
 exports.update = async (request, response) => {
     const language = request.headers.language;
 
-    try {
-        const document = await Model.findOne({ _id: request.body._id });
-        if (!document)
-            if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.params._id}. Document name: {${Model.modelName}}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.params._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
+    const [document, documentError] = await httpHandler.parser(request, response, Model.findOne({ _id: request.body._id }));
+    if (!!documentError) return;
+    if (!document)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.body._id}. Document name: {${Model.modelName}}.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.body._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
 
-        const result = await Model.updateOne({ _id: request.body._id }, request.body, { new: true });
-        if (!result || result.n === 0)
-            if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error updating document with id ${request.params._id}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao atualizar documento de id ${request.params._id}. Nome do documento: {${Model.modelName}}.`);
+    const [data, error] = await httpHandler.parser(request, response, Model.updateOne({ _id: request.body._id }, request.body, { new: true }));
+    if (!!error) return;
+    if (!data || data.n === 0)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error updating document with id ${request.body._id}.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao atualizar documento de id ${request.body._id}. Nome do documento: {${Model.modelName}}.`);
 
-        httpHandler.success(response, result, StatusCode.SuccessNoContent);
-    } catch (error) {
-        if (language == 'en-US') httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-        else httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-    }
+    httpHandler.success(response, data, StatusCode.SuccessNoContent);
 };
 
 exports.delete = async (request, response) => {
     const language = request.headers.language;
 
-    try {
-        const document = await Model.findOne({ _id: request.params._id });
-        if (!document)
-            if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.params._id}. Document name: {${Model.modelName}}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.params._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
+    const [document, documentError] = await httpHandler.parser(request, response, Model.findOne({ _id: request.params._id }));
+    if (!!documentError) return;
+    if (!document)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Document not found with id ${request.params._id}. Document name: {${Model.modelName}}.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorNotFound, `Documento de id ${request.params._id} não encontrada. Nome do documento: {${Model.modelName}}.`);
 
-        const result = await Model.deleteOne({ _id: request.params._id }, request.body);
-        if (!result || result.n === 0)
-            if (language == 'en-US')
-                return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error removing document with id ${request.params._id}. Document name: {${Model.modelName}}.`);
-            else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao remover documento de id ${request.params._id}. Nome do documento: {${Model.modelName}}.`);
+    const [data, error] = await httpHandler.parser(request, response, Model.deleteOne({ _id: request.params._id }, request.body));
+    if (!!error) return;
+    if (!data || data.n === 0)
+        if (language == 'en-US') return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Error removing document with id ${request.params._id}. Document name: {${Model.modelName}}.`);
+        else return httpHandler.error(response, {}, StatusCode.ClientErrorBadRequest, `Erro ao remover documento de id ${request.params._id}. Nome do documento: {${Model.modelName}}.`);
 
-        httpHandler.success(response, result, StatusCode.SuccessNoContent);
-    } catch (error) {
-        if (language == 'en-US') httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-        else httpHandler.error(response, error, StatusCode.ServerErrorInternal);
-    }
+    httpHandler.success(response, data, StatusCode.SuccessNoContent);
 };
