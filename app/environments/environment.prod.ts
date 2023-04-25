428d5c24b17237290b0cb4d3b3a71bd0e1925199
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-04-25 11:44:57 -0300

    enhances theming on darkmode

--- app/environments/environment.prod.ts
+++ app/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
     baseUri: 'http://localhost:3000/api',
-    headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    headers: new HttpHeaders().set('Content-Type', 'application/json')
 };
--- app/environments/environment.ts
+++ app/environments/environment.ts
@@ -1,6 +1,6 @@
-import { HttpHeaders } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
-    baseUri: "http://localhost:3000/api",
-    headers: new HttpHeaders().set("Content-Type", "application/json"),
+    baseUri: 'http://localhost:3000/api',
+    headers: new HttpHeaders().set('Content-Type', 'application/json')
 };
