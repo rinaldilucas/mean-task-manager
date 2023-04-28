16b7c884f6fa2677207318d2ee86320fb5bf7ffe
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-04-28 16:44:30 -0300

    fixes connection string

--- api/config/mongodb.config.ts
+++ api/config/mongodb.config.ts
@@ -1,6 +1,6 @@
 
 class DatabaseConfig {
-    url = 'mongodb://localhost:27017/meantemplatedb';
+    url = 'mongodb://127.0.0.1:27017/meantemplatedb';
     urlProd = 'mongodb://host:password@schema:port/collection';
 }
 
