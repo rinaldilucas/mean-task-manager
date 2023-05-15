f10a1f09bb7fe14b900e15f7284eb00680235524
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-05-15 20:18:25 -0300

    minor fixes

--- app/scripts/validators/email.validator.ts
+++ app/scripts/validators/email.validator.ts
@@ -1,5 +1,5 @@
 import { AbstractControl, AsyncValidatorFn } from '@angular/forms';
-import { first, map, switchMap } from 'rxjs/operators';
+import { delay, first, map, switchMap } from 'rxjs/operators';
 
 import { IQueryResult } from '@scripts/models/queryResult.interface';
 import { IUser } from '@scripts/models/user.interface';
@@ -9,6 +9,7 @@ export class EmailValidator {
     static createValidator (userService: UserService): AsyncValidatorFn {
         return (control: AbstractControl) => {
             return control.valueChanges.pipe(
+                delay(500),
                 switchMap((email: string) => userService.checkIfEmailExists(email)),
                 map((response: IQueryResult<IUser>) => (response?.totalCount > 0 ? { alreadyregistered: true } : null)),
                 first()
