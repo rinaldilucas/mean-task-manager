dbe2dee7843a493a87b9319b587732f6053bac65
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-05-19 16:47:02 -0300

    remove unnecessary prop

--- app/environments/environment.prod.ts
+++ app/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
     baseUri: 'http://localhost:3000/api',
-    headers: new HttpHeaders().set('Content-Type', 'application/json'),
     production: true
 };
