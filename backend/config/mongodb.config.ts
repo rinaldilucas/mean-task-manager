4140ecb2f82e91eb4212ea9cb852bac6664964fe
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-11-04 16:00:31 -0300

    feat: changes to deploy backend on render

--- backend/config/mongodb.config.ts
+++ backend/config/mongodb.config.ts
@@ -1,7 +1,7 @@
 
 class DatabaseConfig {
   url = 'mongodb://127.0.0.1:27017/meantemplatedb';
-  urlProd = 'mongodb://host:password@schema:port/collection';
+  urlProd = process?.env?.MONGODB_HOST;
 }
 
 export default new DatabaseConfig();
--- backend/src/utils/redis.handler.ts
+++ backend/src/utils/redis.handler.ts
@@ -1,8 +1,5 @@
 import redis from 'redis';
 
-let redisHost = process?.env?.REDIS_HOST;
-const environment = process?.env?.NODE_ENV;
-
 export default redis.createClient({
-  url: environment === 'PRODUCTION' ? redisHost : '127.0.0.1',
+  url: process?.env?.REDIS_HOST,
 });
