3c25d15d863ef585dd575377f84529572fe9362d
Lucas Rinaldi <lucasreinaldi@gmail.com>
2023-11-04 16:19:51 -0300

    feat: changes to deploy backend on render

--- backend/config/mongodb.config.ts
+++ backend/config/mongodb.config.ts
@@ -1,7 +1,7 @@
 
 class DatabaseConfig {
   url = 'mongodb://127.0.0.1:27017/meantemplatedb';
-  urlProd = process?.env?.MONGODB_HOST;
+  urlProd = 'mongodb+srv://lucasreinaldi:cosmicc01@cluster0.luitt4v.mongodb.net/meantemplatedb';
 }
 
 export default new DatabaseConfig();
